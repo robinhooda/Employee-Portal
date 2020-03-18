a8b3fa20494d32e11fd9e4edc83bccc56321cea0
Robin Hooda <robin.hooda@thoughtworks.com>
2020-03-19 00:40:14 +0530

    Extra styles added to user component

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-user',
     templateUrl: './user.component.html',
-    styleUrls: ['./user.component.css']
+    styleUrls: ['./user.component.css'],
+    styles: [`
+    h3 {
+            color: red
+        }
+    `]
 })
 
 export class UserComponent {
