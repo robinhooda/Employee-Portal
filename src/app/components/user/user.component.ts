0488ac1afc11f35615945fbc71f128dc39577874
Robin Hooda <robin.hooda@thoughtworks.com>
2020-03-18 23:51:00 +0530

    css file added to the component and component added to the root component

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,8 +1,9 @@
-import { Component, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-user',
-    template: '<h2> User Component</h2>'
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.css']
 })
 
 export class UserComponent {
